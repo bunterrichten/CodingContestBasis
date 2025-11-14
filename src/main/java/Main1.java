0f37ca8bc55abb3b4dfb07c59cbe4df8143a4b77
bunterrichten <niklas@bunterrichten.com>
2025-11-14 12:39:41 +0100

    test 2

--- src/main/java/Main1.java
+++ src/main/java/Main1.java
@@ -3,9 +3,9 @@ import java.util.List;
 
 public class Main1 {
     public static void main(String[] args) {
-        int lvl = 1;
+        int lvl = 1; // umschreiben für level 2, level 3 etc.
 
-        for (int subLvl = 1; subLvl <= 5; subLvl++) {
+        for (int subLvl = 1; subLvl <= 5; subLvl++) { // 1-5 sublevel - wenn du mehr hast, erhöhe die Zahl z.B. auf 8
             try {
                 String path = "lvl" + lvl + "/level" + lvl + "_" + subLvl + ".in";
 
@@ -16,14 +16,18 @@ public class Main1 {
                 for (int i = 1; i < lines.size(); i++) { // start at 1 to ignore 1st line which is just nr of lines
                     String line = lines.get(i);
                     String[] parts = line.split(" ");
+
+
+                    // AB HIER BEGINNT DEIN EIGENTLICHER CODE
                     int x = Integer.parseInt(parts[0]);
                     int y = Integer.parseInt(parts[1]);
 
                     int solution = x / 3 * y;
-                    solutions.add(String.valueOf(solution));
+                    solutions.add(String.valueOf(solution)); // adde zu solutions deine Lösung pro Zeile
+                    // ---------------------------------------
                 }
 
-                System.out.println(solutions);
+                System.out.println(solutions); // DEBUG: damit du siehst was passiert
                 FileUtility.writeFile("lvl" + lvl + "/lvl" + lvl + "_" + subLvl + ".out", solutions, false);
             } catch (Exception e) {
                 e.printStackTrace();
